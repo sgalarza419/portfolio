0b26b1ef3f61a6682377546dbad8748037d2f19d
S Galarza <sgalarza4190@gmail.com>
2021-02-16 00:36:29 -0500

    finished portfolio

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Particles from 'react-particles-js';
 import AboutMe from './components/AboutMe'
 import Services from './components/Services'
 import Experience from './components/Experience'
+import Portfolio from './components/Portfolio';
 
 
 function App() {
@@ -37,6 +38,7 @@ function App() {
     <AboutMe />
     <Services />
     <Experience />
+    <Portfolio />
     </>
   );
 }
